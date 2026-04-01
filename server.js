1723f3d1e24cc4233ee71e7782764d3e4f66e2b1
manikandan-704 <nagarajpriyan2004@gmail.com>
2026-04-01 19:31:25 +0530

    initialize finance backend with Express and MongoDB

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const connectDB = require("./src/config/db.js");
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Finance API is Running....");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
